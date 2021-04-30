9cac961d3f86a06d7626e779be2d9feb213b2659
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-30 22:22:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -37,7 +37,7 @@ class Artist
   def add_song(song)
 
       song.artist = self unless song.artist
-      songs << song unless songs.include?(song)
+      @songs << song unless songs.include?(song)
 
 
   end
