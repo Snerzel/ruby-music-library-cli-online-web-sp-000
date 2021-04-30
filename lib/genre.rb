0ed1c09f129aa7f050b96bd6a36fc080fdc752fd
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-30 19:46:45 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -33,6 +33,14 @@ class Genre
   end
 
   def artists
-    @songs.collect {|s| s.artist}.uniq
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
   end
 end
