0e5426b7f8edfd94d85d39c780096087413abb01
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-30 18:49:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,10 +36,10 @@ class Song
   def genre=(genre)
     genre.songs << self
     @genre = genre
-    #if !(genre.songs.include?(self))
-    #  genre.songs << self
+    if !(genre.songs.include?(self))
+     genre.songs << self
 
-    #end
+    end
   end
 
   def self.find_by_name(name)
