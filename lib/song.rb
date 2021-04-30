edd745c711171b55dbcb0e44320fccdafa3b9915
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-30 23:14:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,13 +31,13 @@ class Song
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self)
+    @artist.add_song(self)
 
   end
 
   def genre=(genre)
     @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
+    @genre.songs << self unless genre.songs.include?(self)
   end
 
   def self.find_by_name(name)
